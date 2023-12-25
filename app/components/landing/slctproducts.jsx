35260cb5ff13beaafd3749ff648c700c8d44de45
adaysha100 <aday.ors55@gmail.com>
2023-12-25 21:57:06 +1300

    worked on hero responsiveness minus the image

--- app/components/landing/slctproducts.jsx
+++ app/components/landing/slctproducts.jsx
@@ -1,7 +1,7 @@
 export default function Slctproducts() {
   return (
-    <main className="comingsoon min-h-screen">
-      <div className="comingsoon-content flex-col">
+    <main className="comingsoon min-h-screen ">
+      <div className="comingsoon-content flex-col ">
         <h2 className="text-5xl italic m-10 text-center">products</h2>
         <h1 className="font-nasa text-8xl text-center">COMING SOON!</h1>
       </div>
